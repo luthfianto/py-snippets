0a234065010e6cf85f35954648dab4c562b5f6cd
Rizky Luthfianto <mrluthfianto@gmail.com>
2021-02-16 17:07:05 +0700

    Update fastapi_async_query_pool.py

--- fastapi_async_query_pool.py
+++ fastapi_async_query_pool.py
@@ -3,8 +3,18 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.on_event("startup")
-async def startup():
+async def _startup():
     import aiomysql
     import secret
     app.state.pool = await aiomysql.create_pool(host=secret.DB_URL, port=3306, user=secret.DB_USERNAME, password=secret.DB_PASSWORD, db=secret.DB_DATABASE)
     print("statup done")
+
+async def _get_query_with_pool(pool):
+    async with await pool.acquire() as conn:
+        async with conn.cursor(aiomysql.DictCursor) as cur:
+            await cur.execute(get_sql_query())
+            return await cur.fetchall()
+
+@app.get("/v1/get_data")
+async def _get_data():
+    return await _get_query_with_pool(app.state.pool)
