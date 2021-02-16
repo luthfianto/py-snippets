c997a0131b8356d9e053a28e6fa80d4296652801
Rizky Luthfianto <mrluthfianto@gmail.com>
2021-02-16 17:04:57 +0700

    Delete async_query.py

--- async_query.py
+++
@@ -1,8 +0,0 @@
-import aiomysql
-import random
-
-async def query_with_pool(pool):
-    async with await pool.acquire() as conn:
-        async with conn.cursor(aiomysql.DictCursor) as cur:
-            await cur.execute(get_sql_query())
-            return await cur.fetchall()
