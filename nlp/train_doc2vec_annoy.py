75d9e192f14f7046ff329f31442e40ab2160ece6
Rizky Luthfianto <mrluthfianto@gmail.com>
2021-03-29 01:06:15 +0700

    Update train_doc2vec_annoy.py

--- nlp/train_doc2vec_annoy.py
+++ nlp/train_doc2vec_annoy.py
@@ -26,3 +26,10 @@ model.save(konstanta.MODEL_PATH)
 from gensim.similarities.annoy import AnnoyIndexer
 annoy_index = AnnoyIndexer(model, 100)
 annoy_index.save(konstanta.ANNOY_IDX)
+
+# read
+
+approximate_neighbors=model.dv.most_similar(879, topn=20, indexer=annoy_index)
+course_ids = [row[0] for row in approximate_neighbors]
+print(course_ids)
+df3[df3.id.isin(course_ids)].reset_index()
